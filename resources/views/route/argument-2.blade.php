6124797b6c8690ece5b59f90fec2757c8bdb3648
Penguin not not <bimacahyo376@gmail.com>
2024-09-12 16:52:59 +0700

    Belajar Route/Routes/Routing

---
+++ resources/views/route/argument-2.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Routes</title>
+</head>
+<body>
+    <h1>{{ $title }}</h1>
+    <h3>{{ $teks }}</h3>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,66 @@
 
 use Illuminate\Support\Facades\Route;
 
+//ROUTE VIEW 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');//contoh menggunakan view blade.php routes
+}); 
+
+Route::get('/basic-routes', function () {
+    return 'Basic Routes Laravel'. 10+1;//hanya menampilkan teks "Basik Routes Laravel11" (tanpa menggunakan views blade.php)
+}); 
+
+Route::view('/view-routes', 'route.view-routes'); //SIMPEL ->sekedar menampilkan halaman saja
+
+#cara menampilkan argument ke view
+#1 tanpa return function atau SIMPEL
+Route::view('/argument-1', 'route.argument-1',
+    [
+        'title' => 'cara mengirimkan argument ke view',
+        'teks' => 'Ok Paham'
+    ]
+); 
+#2 dengan return functtion 
+Route::get('/argument-2', function () {
+    return view('route.argument-2', [
+        'title' => 'cara mengirimkan argument ke view',
+        'teks' => 'Ok Paham'
+    ]);
 });
+
+//Redirect
+Route::redirect('/route/redirect-1', '/route/redirect-2'); //mengalihkan atau menendang pengunjung ke halaman lain semisal= adanya maintenance
+
+//parameter->{id}
+#contoh 1
+Route::get('/param/{id}', function ($id) {
+    return'ini route menggunakan parameter id'. $id; #contoh cara mengakses http://127.0.0.1:8000/param/88
+}); 
+#contoh 2
+Route::get('/parameter/{id}', function($id) {
+    return view('route.param', ['id' => $id]);
+});#contoh cara mengakses http://127.0.0.1:8000/parameter/apasaja
+
+
+//Route prefix
+Route::prefix('page')->group(function () {
+    Route::get('/home', function () {
+        return'ini halaman rumah'; #contoh akses http://127.0.0.1:8000/page/home
+    }); 
+    Route::get('/about', function () {
+        return'ini halaman tentang'; #contoh akses http://127.0.0.1:8000/page/about
+    }); 
+    Route::get('/contact', function () {
+        return'ini halaman kontak'; #contoh akses http://127.0.0.1:8000/page/contact
+    }); 
+});
+
+
+//Methods
+#Route::get(); //mendapatkan data
+#Route::post(); //menginput data ke Database
+#Route::put(); //mengupdate data ke Database
+#Route::patch(); //mengupdate data ke Database
+#Route::delete(); //menghapus data
+
+
